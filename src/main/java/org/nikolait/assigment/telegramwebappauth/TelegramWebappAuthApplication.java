09faa6d38832aa6e20449c58032c3efd034d3392
NikolaiT1986 <kolyan-04031986@mail.ru>
2025-05-29 18:30:52 +0300

    init setup

---
+++ src/main/java/org/nikolait/assigment/telegramwebappauth/TelegramWebappAuthApplication.java
@@ -0,0 +1,13 @@
+package org.nikolait.assigment.telegramwebappauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramWebappAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramWebappAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nikolait/assigment/telegramwebappauth/TelegramWebappAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nikolait.assigment.telegramwebappauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramWebappAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
