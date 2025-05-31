08d4a91edabdb418ad619b8e5eb7f086987b1cd6
NikolaiT1986 <kolyan-04031986@mail.ru>
2025-05-31 22:43:45 +0300

    bugfix: TelegramWebappAuthApplicationTests add TestcontainersConfiguration and test profile

--- src/test/java/org/nikolait/assigment/telegramwebappauth/TelegramWebappAuthApplicationTests.java
+++ src/test/java/org/nikolait/assigment/telegramwebappauth/TelegramWebappAuthApplicationTests.java
@@ -1,9 +1,14 @@
 package org.nikolait.assigment.telegramwebappauth;
 
 import org.junit.jupiter.api.Test;
+import org.nikolait.assigment.telegramwebappauth.config.TestcontainersConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
 class TelegramWebappAuthApplicationTests {
 
     @Test
